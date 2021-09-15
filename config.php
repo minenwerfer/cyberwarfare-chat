5359386850f3c525d7e1162686ad259a0d2898d5
João Santos <joaosan177@gmail.com>
2021-09-15 16:26:44 -0300

    config comments

--- config.php
+++ config.php
@@ -1,11 +1,22 @@
 <?php
 
+// timezone
+define('DEFAULT_TMZ', 'America/Sao_Paulo');
+
+// date formating
+define('DATE_FORMAT', 'j/n/Y G:i\\h');
+
+// where to store settings
 define('SETTINGS_PATH', __DIR__ .'/settings');
+
+// where to store sessions
 define('STORE_PATH', __DIR__ .'/chats');
+
+// crypto
+define('CRYPTO_IV', 'e8957b96915c9266');
+define('CRYPTO_SALT', '99c5c58430c349c7d3647b69cda857e7');
+
+// no need to change below
 define('BASE_DIR', __DIR__);
 define('BASE_URI', dirname($_SERVER['SCRIPT_NAME']));
 define('SANITY_STR', 'sanity_ok');
-define('CRYPTO_IV', 'e8957b96915c9266');
-define('CRYPTO_SALT', '99c5c58430c349c7d3647b69cda857e7');
-define('DEFAULT_TMZ', 'America/Sao_Paulo');
-define('DATE_FORMAT', 'j/n/Y G:i\\h');
